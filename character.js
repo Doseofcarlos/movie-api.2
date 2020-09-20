cc370c28b533d26ca7e065b5e69dd542fbfb3ae9
Adriana <dri.abedala@gmail.com>
2020-09-19 23:14:19 -0400

    add more information from API

--- character.js
+++ character.js
@@ -18,6 +18,11 @@
         //     "Rubeus Hagrid"
         //     ];
 
+        //dobby
+        //Voldemor
+        //minerva 
+        //severa snape
+
         var arrayCharacter = [{
             name : "Harry Potter",
             biog : "Harry James Potter was an England and only child and son of James and Lily Potter, orphaned as an infant. He was a student wizard at Hogwarts School of Witchcraft and Wizardry, where he belongs to the house of Gryffindor together his friends Hermione Granger and Ronald Weasley. He was one of the most famous wizards of modern times."
@@ -45,8 +50,6 @@
           ];
 
       var result = [];
-      
-
       var index = 0; 
 
       for (var iloop = 0; iloop < arrayCharacter.length; iloop++ ){
@@ -86,42 +89,85 @@
 
         for (var iloop = 0; iloop < arrayCharacter.length; iloop++ ){
 
+          //Check name from API is the same from arrayCharacter to create HTML elements
           if(result[0].name == arrayCharacter[iloop].name){
-            var nameCharacter = result[0].name;
 
-            console.log("nameCharacter" + nameCharacter)
+            var nameCharacter = result[0].name;
 
             var title = $("<h2>").text(nameCharacter);
 
             var image = $("<img>").attr("src", "img/" + nameCharacter + ".jpg");
-            image.addClass("img-thumbnail img-fluid");
+            image.addClass("center");
+           
+            //get short description from obj arrayCharacter
+            var bio = $("<p>").text(arrayCharacter[iloop].biog); 
 
-            var bio = $("<p>").text(arrayCharacter[iloop].biog);  
+            var blood = $("<h6>").text("Blood Status: ");
+            var statusblood = $("<span>").text(result[0].bloodStatus); 
+            $(blood).append(statusblood);
+
+            var wandtitle = $("<h6>").text("Wand: ");
+            var wand  = $("<span>").text(result[0].wand);
+            $(wandtitle).append(wand);
+
+            var housetitle = $("<h6>").text("House: ");
+            var house = $("<span>").text(result[0].house);
+            $(housetitle).append(house);
+
+            var roletitle = $("<h6>").text("Role: ");
+            var role = $("<span>").text(result[0].role);
+            $(roletitle).append(role);
+
+
+            // var order = result[0].orderOfThePhoenix;
+            // var patronus = result[0].patronus;
+            // var species = result[0].species;
+            
 
             var parag = $("<p>")
             var btnDetail = $("<button>");
             btnDetail.addClass("btn btn-secondary");
-            btnDetail.attr({"data-toggle":"modal", "data-target":"#exampleModal"});
+            btnDetail.attr({"data-toggle":"modal", "data-target":"#myModal"});
             btnDetail.text("View details >>");
+
             
-            $(btnDetail).on('shown.bs.modal', function () {
+            // console.log(blood);
+            // console.log(wand);
+            // console.log(house);
+
+            //shown.bs.modal
+            //$(btnDetail).on('click', function () {
+            $("#myBtn_3").on('click', function () {
+
               event.preventDefault();
-              DetailProfile(nameCharacter);
+              // var bioname = $("<p>").text("Name: " + nameCharacter);
+              // var bioblood = $("<p>").text("Blood: " + blood) 
+             
+              // $("#biomodal").append(bioname, bioblood);
+              DetailProfile(nameCharacter, blood);
+   
+              //$("#myModal").modal("show");
+              
+         
             });
 
             parag.append(btnDetail);
           
-            $("#column_" + index).append(image, title, bio, parag); 
+            $("#column_" + index).append(image, title, bio, blood, wandtitle, housetitle, roletitle); 
 
           }
         }
         index = index +1;
     }
 
-    function DetailProfile(name){
-      //var params = "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no, width=600,height=300,left=100,top=100";
-      //open('detailProfile.html?name=' + name, 'Profile', params);
+    function DetailProfile(name, blood){
+      var params = "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no, width=600,height=300,left=100,top=100";
+      open('detailProfile.html?name=' + name, 'Profile', params);
 
+      // var bioname = $("<p>").text("Name: " + nameCharacter);
+      // var bioblood = $("<p>").text("Blood: " + blood) 
+      // $("#biomodal").prepend(bioname, bioblood);
+      // $("#myModal").modal();
 
 
     }
